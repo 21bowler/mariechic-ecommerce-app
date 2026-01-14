e1d1cfc1bd83c6015ce5af305e6e01e71975ba91
21bowler <samwachira224@gmail.com>
2026-01-14 22:16:11 +0300

    add various components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
+import Hero from "@/components/home/Hero";
+import Features from "@/components/home/Features";
+import Categories from "@/components/home/Categories";
+import BestSellers from "@/components/home/BestSellers";
+import PromoBanner from "@/components/home/PromoBanner";
+import NewArrivals from "@/components/home/NewArrivals";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -9,7 +14,12 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Features />
+        <Categories />
+        <BestSellers />
+        <PromoBanner />
+        <NewArrivals />
       </main>
+      <Footer />
     </div>
   );
 }
