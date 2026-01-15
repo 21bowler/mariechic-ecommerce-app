452e44052617a0bf6dac7cadc384cd156cf52af3
21bowler <samwachira224@gmail.com>
2026-01-15 21:37:39 +0300

    add:slider and checkbox

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,25 +1,19 @@
-import Header from "@/components/Header";
 import Hero from "@/components/home/Hero";
 import Features from "@/components/home/Features";
 import Categories from "@/components/home/Categories";
 import BestSellers from "@/components/home/BestSellers";
 import PromoBanner from "@/components/home/PromoBanner";
 import NewArrivals from "@/components/home/NewArrivals";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <Features />
-        <Categories />
-        <BestSellers />
-        <PromoBanner />
-        <NewArrivals />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <Features />
+      <Categories />
+      <BestSellers />
+      <PromoBanner />
+      <NewArrivals />
+    </>
   );
 }
