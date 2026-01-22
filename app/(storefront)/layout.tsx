79caaeabd87a2ac63abb0fd248596fbc6a381f82
21bowler <samwachira224@gmail.com>
2026-01-22 14:42:46 +0300

    fix:proper layout name rewritten

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-export default function ShopLayout({
+export default function StorefrontLayout({
   children,
 }: {
   children: React.ReactNode;
