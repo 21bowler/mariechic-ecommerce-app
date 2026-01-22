b3ac434109a69780fe444a5489f2d386fc872444
21bowler <samwachira224@gmail.com>
2026-01-22 16:11:20 +0300

    feat(utils):add formatPrice and calculateDiscount helpers

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// formatter helper for Kenyan Shillings
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat("en-KE", {
+    style: "currency",
+    currency: "KES",
+    minimumFractionDigits: 0,
+  }).format(price);
+};
+
+// Discount Calculation Helper function
+export const calculateDiscount = (
+  originalPrice: number | undefined,
+  price: number,
+): number => {
+  if (originalPrice) {
+    return Math.round(((originalPrice - price) / originalPrice) * 100);
+  }
+  return 0;
+};
