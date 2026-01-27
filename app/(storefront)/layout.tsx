efcd9b6c2ec9fde984105d4e56a829f04e54c5a2
21bowler <samwachira224@gmail.com>
2026-01-27 22:53:04 +0300

    fix(root-layout): add the cartdrawer to the layout for consistency.

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import CartDrawer from "@/components/cart/CartDrawer";
 
 export default function StorefrontLayout({
   children,
@@ -10,6 +11,7 @@ export default function StorefrontLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <CartDrawer />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
