44cff6f04e2ba0ad06ec97b608dbd000d44f920a
21bowler <samwachira224@gmail.com>
2026-01-28 21:08:13 +0300

    feat(checkout-page): add a Checkout form on the server-side page

---
+++ app/(storefront)/checkout/page.tsx
@@ -0,0 +1,12 @@
+import { ChevronLeft } from "lucide-react";
+import CheckoutFormClient from "@/components/checkout/CheckoutFormClient";
+
+const CheckoutPage = () => {
+  return (
+    <div>
+      <CheckoutFormClient />
+    </div>
+  );
+};
+
+export default CheckoutPage;
